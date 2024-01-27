9a61832025baae81d70e59c5529a8ab5b5f7d0c7
Sahil-Chhoker <chhokersahil435@gmail.com>
2024-01-27 06:45:21 -0500

    Getting our server online

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Sahil-Chhoker/rssagg
 
 go 1.21.6
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server staring on port :%v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("PORT : ", portString)
