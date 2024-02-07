fb28cd0374d5a26fe8e6bd1026e06bd8b39ec3dc
Ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2024-02-07 18:39:22 +0530

    added a bad request handler script

--- handler_readiness.go
+++ handler_readiness.go
@@ -6,6 +6,6 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 }
 
-func handlerErr(w http.ResponseWriter, r *http.Request) {
-	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
-}
+// func handlerErr(w http.ResponseWriter, r *http.Request) {
+// 	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+// }
